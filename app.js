8105f6693731f4c9a6d25286a5da2f0b88b92639
Fel <FelWebDev@gmail.com>
2020-10-26 19:28:10 -0600

    Created Controller and Route files for Bootcamp Routes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route Files
+const bootcamps = require('./routes/bootcamps');
+
 // Load Env Variables
 dotenv.config({ path: './config/config.env' });
 
+// Initialize Express App
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(
     PORT, 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
