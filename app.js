5d6609cbc8d8ccf9c9f7a5d4d360e86c3850145b
Fel <FelWebDev@gmail.com>
2020-10-27 20:29:01 -0600

    Connected to MongoDB w/ Mongoose and created BootCampSchema

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route Files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load Env Variables
 dotenv.config({ path: './config/config.env' });
 
+// Connect to Database
+connectDB();
+
+// Route Files
+const bootcamps = require('./routes/bootcamps');
+
 // Initialize Express App
 const app = express();
+
+// Dev Logging Middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-
-
+// Port Open
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
     PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server and Exit Process
+    server.close(() => {
+        process.exit(1);
+    });
+})
